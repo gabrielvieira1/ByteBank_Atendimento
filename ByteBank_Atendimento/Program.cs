73cb2d930488be08aeac74e88b82423bf663062d
Gabriel Vieira <gabrielvieira1@protonmail.com>
2023-01-07 19:59:59 -0300

    Adicionando DLL Gerador de PIX

--- ByteBank_Atendimento/Program.cs
+++ ByteBank_Atendimento/Program.cs
@@ -1,3 +1,14 @@
 ﻿using ByteBank_Atendimento.bytebank.Atendimento;
+using Bytebank_GeradorChavePix;
+
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
-new ByteBankAtendimento().AtendimentoCliente();
+//new ByteBankAtendimento().AtendimentoCliente();
+
+Console.WriteLine(GeradorPix.GetChavePix());
+
+var listaDeChaves = GeradorPix.GetChavesPix(10);
+
+foreach (var chave in listaDeChaves)
+{
+  Console.WriteLine(chave);
+}
